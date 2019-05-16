09e250c75ee9bf4928ec9a2765d546813875e74c
Molin <molin.yang.gr@dartmouth.edu>
2019-05-16 16:20:40 -0400

    DartLife

---
+++ app/src/main/java/com/example/dartlife/fragment/FoodFragment.java
@@ -0,0 +1,22 @@
+package com.example.dartlife.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dartlife.R;
+
+public class FoodFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_food, container, false);
+    }
+}
+
